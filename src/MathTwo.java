7cc63c7bd0fdaf8329261462ef8463183266deb0
deethompson <devon.thompson0@gmail.com>
2019-09-04 16:49:15 -0400

    math two assignment

---
+++ src/MathTwo.java
@@ -0,0 +1,26 @@
+
+
+
+public class  MathTwo{
+    public static void main ( String[] args) {
+        //declare variables here before we use them
+        int x;
+        double y;
+        //all three variables will be declared as integers
+
+        //assign values to the variables here
+        x = 2;
+
+        y = 1.7938; //you could also use the constant Integer.MAX_VALUE
+
+
+
+        //Use the variables here
+        System.out.println( "The variable x contains a value of " + x );
+        System.out.print( "The value" + y + "is the largest value");
+        System.out.println( "you can store in an integer.");
+
+
+
+    }
+}
